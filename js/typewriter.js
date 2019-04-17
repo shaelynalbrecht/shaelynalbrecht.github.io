e476468c11c00988d88493fd920f98579967347e
Shaelyn Albrecht <shaelyn@Shaelyns-MacBook-Pro.local>
2019-04-17 11:53:18 -0400

    typewriter

---
+++ js/typewriter.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'Hello! Welcome to a website dedicated to the sport of baton twirling.'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("greeting").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}

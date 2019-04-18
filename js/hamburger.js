25a6932ffe5bd16a445a61e1d5ccf785900cfd0e
Shaelyn Albrecht <shaelyn@0587378860.wireless.umich.net>
2019-04-17 22:49:58 -0400

    ugh

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,18 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function hamburgerFunction() {
   var x = document.getElementById("links");
-  if (x.style.display === "block") {
+  var icon = document.querySelector(".icon");
+  if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
     x.style.display = "flex";
     x.style.flexDirection = "column";
+    x.style.marginTop = "10%";
   }
 }
+// let mainNav = document.getElementById('links');
+// let navBarToggle = document.getElementById('js-navbar-toggle');
+//
+// navBarToggle.addEventListener('click', function () {
+//   mainNav.classList.toggle('active');
+// });

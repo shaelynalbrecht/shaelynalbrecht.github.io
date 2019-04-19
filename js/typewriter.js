2aa45ea82aaf6eabde970482b1e80d64c3231681
Shaelyn Albrecht <shaelyn@Shaelyns-MacBook-Pro.local>
2019-04-19 09:52:32 -0400

    fixed lots

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,6 +1,7 @@
 var i = 0;
-var txt = 'Hello! Welcome to a website dedicated to the sport of baton twirling.'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+var txt = 'Hello! Welcome to a website dedicated to the sport of baton twirling.';
+ /* The text */
+var speed = 70; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
